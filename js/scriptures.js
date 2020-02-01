ec7fbaefaa606d8828886e272d14c7bcea7064e5
Adam-Peterson <adpete44@gmail.com>
2020-02-01 15:33:35 -0700

    end of video 5

--- js/scriptures.js
+++ js/scriptures.js
@@ -11,7 +11,7 @@
     slice, split, status, title, tocName
 */
 
-const Scriptures = (function ()
+const Scriptures =(function()
 {
     "use strict";
 
@@ -26,17 +26,21 @@ const Scriptures = (function ()
     // PRIVATE METHOD DECLARATIONS
     let init;
     let ajax;
+    let bookChapterValid;
+    let navigateChapter;
     let cacheBooks;
     let onHashChanged;
+    let navigateHome;
+    let navigateBook;
 
 
     // PRIVATE METHODS
-    ajax = function (url, successCallback, failureCallback)
+    ajax = function(url, successCallback, failureCallback)
     {
         let request = new XMLHttpRequest();
         request.open("GET", url, true);
 
-        request.onload = function ()
+        request.onload = function()
         {
             if(request.status >= 200 && request.status < 400)
             {
@@ -60,15 +64,35 @@ const Scriptures = (function ()
         request.send();
     };
 
+    navigateChapter = function(bookId, chapter)
+    {
+        console.log(bookId, chapter);
+    };
+
+    bookChapterValid = function(bookId, chapter)
+    {
+        let book = books[bookId];
+
+        if(book === undefined || chapter < 0 || chapter > book.numChapters)
+        {
+            return false;
+        }
+        if(chapter === 0 && book.numChapters > 0)
+        {
+            return false;
+        }
+
+        return true;
+    };
 
     cacheBooks = function(onInitializedCallback)
     {
-        volumes.forEach(function (volume)
+        volumes.forEach(function(volume)
         {
             let volumeBooks = [];
             let bookId = volume.minBookId;
 
-            while (bookId <= volume.maxBookId)
+            while(bookId <= volume.maxBookId)
             {
                 volumeBooks.push(books[bookId]);
                 bookId += 1;
@@ -82,12 +106,13 @@ const Scriptures = (function ()
         }
     };
 
-    init = function (onInitializedCallback)
+    init = function(onInitializedCallback)
     {
+        console.log("Starting init...");
         let booksLoaded = false;
         let volumesLoaded = false;
 
-        ajax("https://scriptures.byu.edu/mapscrip/model/books.php", function (data)
+        ajax("https://scriptures.byu.edu/mapscrip/model/books.php", function(data)
         {
             books = data;
             booksLoaded = true;
@@ -98,7 +123,7 @@ const Scriptures = (function ()
             }
         });
 
-        ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", function (data)
+        ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", function(data)
         {
             volumes = data;
             volumesLoaded = true;
@@ -110,10 +135,76 @@ const Scriptures = (function ()
         });
     };
 
-    onHashChanged = function ()
+    navigateBook = function(bookId)
     {
-        console.log("Hash" + location.hash);
-    }
+        console.log(bookId);
+    };
+
+    navigateHome = function(volumeId)
+    {
+        document.getElementById("scriptures").innerHTML = 
+        "<div>OT</div>" + 
+        "<div>NT</div>" + 
+        "<div>BOM</div>" + 
+        "<div>DC</div>" + 
+        "<div>POGP</div>" +
+        volumeId;
+    };
+
+    onHashChanged = function()
+    {
+        let ids = [];
+
+        if(location.hash !== "" && location.hash.length > 1)
+        {
+            ids = location.hash.slice(1).split(":");
+        }
+        if(ids.length <= 0)
+        {
+            navigateHome();
+        }
+        else if(ids.length === 1)
+        {
+            let volumeId = Number(ids[0]);
+
+            if(volumeId < volumes[0].id || volumeId > volumes.slice(-1).id)
+            {
+                navigateHome()
+            }
+            else
+            {
+                navigateHome(volumeId);
+            }
+        }
+        else if(ids.length >= 2)
+        {
+            let bookId = Number(ids[1]);
+            if(books[bookId] === undefined)
+            {
+                navigateHome();
+            }
+            else
+            {
+                if(ids.length === 2)
+                {
+                    navigateBook(bookId);
+                }
+                else
+                {
+                    let chapter = Number(ids[2]);
+
+                    if(bookChapterValid(bookId, chapter))
+                    {
+                        navigateChapter(bookId, chapter)
+                    }
+                    else
+                    {
+                        navigateHome();
+                    }
+                }
+            }
+        }
+    };
 
     // PUBLIC API
     return {
